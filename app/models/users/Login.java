fbbb2201dfd20036a6cfdf03e5bc0881e0a8d27d
Enda Lee <enda.lee@ittdublin.ie>
2016-12-09 11:13:47 +0000

    details of commit

---
+++ app/models/users/Login.java
@@ -0,0 +1,37 @@
+package models.users;
+
+/**
+ * Created by wdd on 09/12/16.
+ */
+public class Login {
+
+
+    private String email;
+    private String password;
+
+
+    public String validate(){
+
+        if(User.authenticate(getEmail(), getPassword()) == null){
+            return "Invalid user or password";
+        }
+        return null;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
